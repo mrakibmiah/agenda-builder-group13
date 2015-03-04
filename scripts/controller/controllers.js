df8b1dfd292a120ec2a8842d6c1f056302718b87
rakibuiu <rakibuiu@yahoo.com>
2015-03-04 17:30:49 +0100

    New interface Design and tweaking CSS & HTML

--- scripts/controller/controllers.js
+++ scripts/controller/controllers.js
@@ -3,18 +3,18 @@ var maincontrollerModule = angular.module('maincontrollerModule', ["modelModule"
 maincontrollerModule.controller('MainCtrl', ['$scope', 'ngmodel', function ($scope, ngmodel) {
         $scope.tomas = 'age';
         $scope.model = ngmodel.results; // get the model object    
-        $scope.numberOfcolumns = $scope.model.days.length + 1;
+        $scope.numberOfcolumns = $scope.model.days.length;
 
         $scope.addDay = function () {
             $scope.model.addDay();
-            $scope.numberOfcolumns = $scope.model.days.length + 1;
+            $scope.numberOfcolumns = $scope.model.days.length;
             console.log($scope.numberOfcolumns);
         }
 
         $scope.removeDay = function (index) {
             if (confirm('Are you sure you want to delete this? Warning!! All actvities belong to this day will be deleted too.')) {
                 $scope.model.removeDay(index);
-                $scope.numberOfcolumns = $scope.model.days.length + 1;
+                $scope.numberOfcolumns = $scope.model.days.length;
             }
 
             //console.log($scope.numberOfcolumns);
@@ -53,8 +53,6 @@ angular.module('dragAndDropControllerModule', ['ui.sortable', "modelModule"]).
 var ngBootstrapUIModule = angular.module('ngBootstrapUIModule', ['ui.bootstrap', 'modelModule']);
 ngBootstrapUIModule.controller('ModalCtrl', ['$scope', '$modal', '$log', 'ngmodel', function ($scope, $modal, $log, ngmodel) {
         $scope.options = ngmodel.results.activityType;
-        $scope.colorIndex = 0;
-        // $scope.correctlySelected = $scope.options[0];
         $scope.open = function (size) {
             var modalInstance = $modal.open({
                 templateUrl: 'myModalContent.html',
@@ -63,10 +61,6 @@ ngBootstrapUIModule.controller('ModalCtrl', ['$scope', '$modal', '$log', 'ngmode
                 resolve: {
                     options: function () {
                         return $scope.options;
-                    },
-                    colorIndex: function () {
-                        $scope.colorIndex++;
-                        return $scope.colorIndex;
                     }
                 }
             });
@@ -80,22 +74,27 @@ ngBootstrapUIModule.controller('ModalCtrl', ['$scope', '$modal', '$log', 'ngmode
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
-ngBootstrapUIModule.controller('ModalInstanceCtrl', ["$scope", "$modalInstance", "options", "colorIndex", "ngmodel", function ($scope, $modalInstance, options, colorIndex, ngmodel) {
+ngBootstrapUIModule.controller('ModalInstanceCtrl', ["$scope", "$modalInstance", "options", "ngmodel", function ($scope, $modalInstance, options, ngmodel) {
         $scope.options = options;
         $scope.activityType = options[0];
         $scope.model = ngmodel.results;
 
-
         $scope.ok = function () {
-            $scope.color = $scope.model.colors[colorIndex];
+            $scope.color = $scope.getRandomColor(5);
             // alert($scope.activityType.value);
             $scope.model.addActivity(new Activity($scope.activityName, Number($scope.activityDuration), $scope.activityType.value, $scope.activityDesc, $scope.color));
             // console.log($scope.model.parkedActivities[0].getName());
             $modalInstance.close();
-            //$modalInstance.close($scope.selected.item);   
-
-            console.log(colorIndex);
+            //$modalInstance.close($scope.selected.item);  
         };
+        $scope.getRandomColor = function (brightness) {
+            var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
+            var mix = [brightness * 51, brightness * 51, brightness * 51]; //51 => 255/5
+            var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(function (x) {
+                return Math.round(x / 2.0)
+            })
+            return "rgb(" + mixedrgb.join(",") + ")";
+        }
 
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
